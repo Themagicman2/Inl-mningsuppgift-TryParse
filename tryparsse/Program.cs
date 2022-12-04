93cd1fd3c2ce957523e89f691ec413a41a20abad
japo04001 <89467411+japo04001@users.noreply.github.com>
2022-12-04 17:24:59 +0100

    Add files via upload

---
+++ tryparsse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace tryparsse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = { null, "23145", "24155.6", "21,425",
+                          "-244", "+46423", "(167);", "231FA" };
+            foreach (string value in values)
+            {
+                int nummer;
+
+                bool funkar = int.TryParse(value, out nummer);
+                if (funkar)
+                {
+                    Console.WriteLine($"Converted '{value}' to {nummer}.");
+                }
+                else
+                {
+                    Console.WriteLine($"Attempted conversion of '{value ?? "<null>"}' failed.");
+                }
+            }
+        }
+    }
+}
